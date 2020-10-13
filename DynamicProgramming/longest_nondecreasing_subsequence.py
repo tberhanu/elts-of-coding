a204e98008924ee1e38e2ad6c5b9d65b67436f9c
Tesfahun Berhanu <tberhanu@berkeley.edu>
2020-10-12 23:44:21 -0700

    DP to Greedy

--- DynamicProgramming/longest_nondecreasing_subsequence.py
+++ DynamicProgramming/longest_nondecreasing_subsequence.py
@@ -7,6 +7,92 @@ Longest: [0, 4, 10, 14] or [0, 2, 6, 9]
 
 Hint: Express the longest nondecreasing subsequence ending at entry in terms of the longest nondecreasing
 subsequence appearing in the subarray consisting of preceding elements.
+
+Strategy:
+    1. Have a dictionary: the arr index as a KEY and the longest subsequence ENDING AT that index as a VALUE.
+    2. By default, fill out all the values of the dictionary as 1 since each letter has at least 1 long subsequent.
+    3. Since the max length of subsequent ending at index 0 is always 1, skip it and start from index j = 1.
+    4. So, start the ptr i from 0, and count if any elt arr[i] <= the arr[j], and if the corresponding subsequent
+       values, lookup[i] >= lookup[j], then increment the lookup[j] by one. N.B: Everytime you update the lookup[j],
+       make sure to update the prev_subsequent_index[j] so that you can track the previous index or from where it comes.
+    5. Sort the lookup_subseq_length by the VALUE, and get the highest value INDEX.
+    6. Once getting the highest value INDEX, track the previous indices index by index, and get all the subsequent
+       elements.
 """
-def longest_nondecreasing_subsequence_length(A):
-    pass
+def longest_increasing_subsequents(arr):
+    lookup_subsequent_length = {i: 1 for i, e in enumerate(arr)}
+    prev_subsequent_index = [None for _ in range(len(arr))]
+    i, j = 0, 1
+    while j < len(arr):
+        left = arr[i]
+        right = arr[j]
+        if left < right: # left <= right if you want the longest nondecreasing subsequents
+            if lookup_subsequent_length[i] >= lookup_subsequent_length[j]:
+                lookup_subsequent_length[j] += 1
+                prev_subsequent_index[j] = i
+        i += 1
+        if i == j:
+            i = 0
+            j += 1
+    sorted_keys_by_longer_subsequent = sorted(lookup_subsequent_length,
+                                              key=lambda key: lookup_subsequent_length[key],
+                                              reverse=True)
+    index = sorted_keys_by_longer_subsequent[0]
+    subsequents = []
+    while True:
+        subsequents = [arr[index]] + subsequents
+        index = prev_subsequent_index[index]
+        if index is None:
+            break
+        if index == 0:
+            subsequents = [arr[index]] + subsequents
+            break
+
+    return subsequents
+
+def longest_nondecreasing_subsequents(arr):
+    lookup_subsequent_length = {i: 1 for i, e in enumerate(arr)}
+    prev_subsequent_index = [None for _ in range(len(arr))]
+    i, j = 0, 1
+    while j < len(arr):
+        left = arr[i]
+        right = arr[j]
+        if left <= right:
+            if lookup_subsequent_length[i] >= lookup_subsequent_length[j]:
+                lookup_subsequent_length[j] += 1
+                prev_subsequent_index[j] = i
+        i += 1
+        if i == j:
+            i = 0
+            j += 1
+    sorted_keys_by_longer_subsequent = sorted(lookup_subsequent_length,
+                                              key=lambda key: lookup_subsequent_length[key],
+                                              reverse=True)
+    index = sorted_keys_by_longer_subsequent[0]
+    subsequents = []
+    while True:
+        subsequents = [arr[index]] + subsequents
+        index = prev_subsequent_index[index]
+        if index is None:
+            break
+        if index == 0:
+            subsequents = [arr[index]] + subsequents
+            break
+
+    return subsequents
+
+
+if __name__ == "__main__":
+    arr = [3, 4, -1, 0, 6, 2, 3]
+    longest_subsequent = longest_increasing_subsequents(arr)
+    print(longest_subsequent)
+
+    arr = [3, 4, -1, 0, 0, 6, 2, 3]
+    print(longest_nondecreasing_subsequents(arr))
+
+    arr = [0, 4, 12, 2, 10, 6, 6, 9, 13, 3, 11, 7, 15]
+    longest_subsequents = longest_increasing_subsequents(arr)
+    print(longest_subsequents)
+
+    arr = [0, 4, 12, 2, 10, 6, 6, 9, 13, 3, 11, 7, 15]
+    print(longest_nondecreasing_subsequents(arr))
--- DynamicProgramming/pretty_printing.py
+++ DynamicProgramming/pretty_printing.py
@@ -5,7 +5,7 @@ Consider the problem of laying out text using a fixed width font. Each line can 
 characters. Words on a line are to be separated by exactly one blank. Therefore, we may be left with whitespace at the
 end of a line(since the next word will not fit in the remaining space). This whitespace is visually unappealing.
 Define the messiness of the end-of-line whitespace as follows. The messiness of a single line ending with B blank
-characters is B ** 2. The toatal messiness of a sequence of lines is the sum of the messinesses of all the lines. A
+characters is B ** 2. The total messiness of a sequence of lines is the sum of the messinesses of all the lines. A
 sequence of words can be split across lines in different ways with different messiness.
 Given text, i.e. a string of words separated by single blanks, decompose the text into lines such that no word is split
 across lines and the messiness of the decomposition is minimized. Each line can hold no more than a specified number
@@ -15,5 +15,60 @@ Hint: Focus on the last word and the last line.
 
 """
 def minimum_messiness(words, line_length):
-    pass
+    lines = [[] for _ in range(len(words))]
+    lines[0] = [words[0]]
+    print(lines)
+    num_remaining_blanks = line_length - len(words[0])
+    min_messiness = ([num_remaining_blanks ** 2] + [float('inf')] * (len(words) - 1))
+    print(min_messiness)
+
+
+
+    for i in range(1, len(words)):
+        num_remaining_blanks = line_length - len(words[i])
+        min_messiness[i] = min_messiness[i - 1] + num_remaining_blanks ** 2
+        for j in reversed(range(i)):
+            num_remaining_blanks = num_remaining_blanks - (len(words[j]) + 1)
+            if num_remaining_blanks < 0:
+                break
+            if j - 1 < 0:
+                prev_prev_messiness = 0
+            else:
+                prev_prev_messiness = min_messiness[j - 1]
+
+            current_line_messiness = num_remaining_blanks ** 2
+
+            if min_messiness[i] > prev_prev_messiness + current_line_messiness:
+                min_messiness[i] = prev_prev_messiness + current_line_messiness
+                lines[i] = words[j: i+1]
+
+    print("lines: ", lines)
+    best_lines = []
+    i = len(lines) - 1
+    while i >= 0:
+        if not lines[i]:
+            i -= 1
+            continue
+        best_lines.append(lines[i])
+        if i - 1 >= 0 and lines[i][0] == lines[i - 1][-1]:
+            i -= 2
+        else:
+            i -= 1
+
+    answer = [" ".join(e) for e in best_lines[::-1]]
+    print(answer)
+    print("b: ", best_lines)
+    return min_messiness[-1]
+
+
+
+if __name__ == "__main__":
+    # words = ["Tushar", "Roy", "likes", "to", "code"]
+    # line_length = 10
+    # m_m = minimum_messiness(words, line_length)
+    # print(m_m)
+
+    words = ["aaa", "bbb", "c", "d", "ee", "ff", "ggggggg"]
+    mm = minimum_messiness(words, 10)
+    print(mm)
 
